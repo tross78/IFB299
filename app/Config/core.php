c2ba099da2cce52bf9bc464b68779fb99468d4a0
Tyson Ross <tross_github@tysonross.com>
2016-08-20 21:38:19 +1000

    Defaulting debug to zero, setting from ENV not working

--- app/Config/core.php
+++ app/Config/core.php
@@ -63,7 +63,7 @@ if (!env('APP_NAME')) {
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', intval(getenv('DEBUG')));
+	Configure::write('debug', 0);
 
 /**
  * Configure the Error handler used to handle errors for your application. By default
