7655f64f9902fa4903d398a41506cec52ef8e8d0
Tyson Ross <tross_github@tysonross.com>
2016-08-20 21:03:20 +1000

    blah

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -76,8 +76,7 @@ App::build(
  * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
  *
  */
-CakePlugin::load('Crud');
-CakePlugin::load('DebugKit');
+CakePlugin::loadAll();
 
 if (php_sapi_name() !== 'cli' && Configure::read('debug') && in_array('DebugKit', App::objects('plugin'))) {
 	CakePlugin::load('DebugKit');
