7992c0eb2be118624877d043dab3ac1dc0939dd5
Harrison Gee <harrison@Harrisons-MacBook-Air.local>
2016-08-31 10:12:14 +1000

    trying to fix what i broke

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -31,12 +31,11 @@
  * ...and connect the rest of 'Pages' controller's urls.
  */
 	Router::connect('/pages/*', ['controller' => 'pages', 'action' => 'display']);
-
+/**
  * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
  */
 	CakePlugin::routes();
-
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
  * the built-in default routes.
