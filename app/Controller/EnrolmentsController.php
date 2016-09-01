df78ada2d725925dbb1eb5196edc8c37ac18e932
Tyson Ross <tross_github@tysonross.com>
2016-09-01 22:29:05 +1000

    ###

--- app/Controller/EnrolmentsController.php
+++ app/Controller/EnrolmentsController.php
@@ -42,7 +42,7 @@ class EnrolmentsController extends AppController {
 		
 
 		$options = array('conditions' => array('Enrolment.' . $this->Enrolment->primaryKey => $id));
-		$this->set('enrolment', $this->Enrolment->find('all', $options));
+		$this->set('enrolment', $this->Enrolment->find('list', $options));
 		//$enrolment = $this->Enrolment->Enrolment->find('list');
 		//$students = $this->Enrolment->Student->find('list');
 		//$courses = $this->Enrolment->Course->find('list');
