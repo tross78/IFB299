d43721555af5691843175839d149cc1e1a9e1e38
Tyson Ross <tross_github@tysonross.com>
2016-09-01 21:50:18 +1000

    ###

--- app/Controller/EnrolmentsController.php
+++ app/Controller/EnrolmentsController.php
@@ -38,12 +38,15 @@ class EnrolmentsController extends AppController {
 		}
 		//$this->set('enrolments', $this->Paginator->paginate());
 		//$options = array('conditions' => array('Enrolment.' . $this->Enrolment->primaryKey => $id));
-		$options = array('conditions' => array('Enrolment.' . $this->Enrolment->primaryKey => $id));
-		$this->set('enrolment', $this->Enrolment->find('all', $options));
+		//$options = array('conditions' => array('Enrolment.' . $this->Enrolment->primaryKey => $id));
+		//$this->set('enrolment', $this->Enrolment->find('all', $options));
 		//$enrolment = $this->Enrolment->Enrolment->find('list');
 		//$students = $this->Enrolment->Student->find('list');
 		//$courses = $this->Enrolment->Course->find('list');
 		//$this->set(compact('students', 'courses'));
+		$students = $this->Enrolment->Student->find('list');
+		$courses = $this->Enrolment->Course->find('list');
+		$this->set(compact('students', 'courses'));
 	}
 
 /**
