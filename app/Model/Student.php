c9cfecd9a22b7cad35c6c14981a022aaba6edb92
Tyson Ross <tross_github@tysonross.com>
2016-09-01 17:10:53 +1000

    Fix for Student null msg when submitting

--- app/Model/Student.php
+++ app/Model/Student.php
@@ -94,16 +94,6 @@ class Student extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
-		'dietary_requirements' => array(
-			'notBlank' => array(
-				'rule' => array('notBlank'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 	);
 
 	// The Associations below have been created with all possible keys, those that are not needed can be removed
