7529a271d00738ad4b4246d3b06538feb111efba
Tyson Ross <tross_github@tysonross.com>
2016-09-02 13:06:39 +1000

    Deleted Roll from the form

--- app/Model/Student.php
+++ app/Model/Student.php
@@ -38,16 +38,6 @@ class Student extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
-		'role' => array(
-			'notBlank' => array(
-				'rule' => array('notBlank'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 		'first_name' => array(
 			'notBlank' => array(
 				'rule' => array('notBlank'),
