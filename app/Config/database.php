c26f9129fcd2db84c6a8014c5e8de951d87e3671
Tyson Ross <tross_github@tysonross.com>
2016-09-03 19:00:02 +1000

    Redid DB vars

--- app/Config/database.php
+++ app/Config/database.php
@@ -4,6 +4,16 @@ use AD7six\Dsn\Wrapper\CakePHP\V2\DbDsn;
 
 class DATABASE_CONFIG {
 
+	public $default = array(
+        'datasource'  => 'Database/Mysql',
+        'persistent'  => false,
+        'host'        => 'us-cdbr-iron-east-04.cleardb.net',
+        'login'       => 'b9499b0fbead53',
+        'password'    => '3ce83671',
+        'database'    => 'heroku_2dfb529ea745083',
+        'prefix'      => ''
+    );
+    
 // /**
 //  * Define connections using environment variables
 //  *
@@ -14,13 +24,4 @@ class DATABASE_CONFIG {
 		$this->test = DbDsn::parse(env('DATABASE_TEST_URL'));
 	}
 
-	public $default = array(
-        'datasource'  => 'Database/Mysql',
-        'persistent'  => false,
-        'host'        => 'us-cdbr-iron-east-04.cleardb.net',
-        'login'       => 'b9499b0fbead53',
-        'password'    => '3ce83671',
-        'database'    => 'heroku_2dfb529ea745083',
-        'prefix'      => ''
-    );
 }
