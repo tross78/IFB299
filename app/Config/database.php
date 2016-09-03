e00484ba7834d8ea7a24ba26bdd185a36724c8b1
Tyson Ross <tross_github@tysonross.com>
2016-09-03 18:54:38 +1000

    added default DB details for local use

--- app/Config/database.php
+++ app/Config/database.php
@@ -4,13 +4,23 @@ use AD7six\Dsn\Wrapper\CakePHP\V2\DbDsn;
 
 class DATABASE_CONFIG {
 
-/**
- * Define connections using environment variables
- *
- * @return void
- */
+// /**
+//  * Define connections using environment variables
+//  *
+//  * @return void
+//  */
 	public function __construct() {
 		$this->default = DbDsn::parse(env('DATABASE_URL'));
 		$this->test = DbDsn::parse(env('DATABASE_TEST_URL'));
 	}
+
+	public $default = array(
+        'datasource'  => 'Database/Mysql',
+        'persistent'  => false,
+        'host'        => 'us-cdbr-iron-east-04.cleardb.net',
+        'login'       => 'b9499b0fbead53',
+        'password'    => '3ce83671',
+        'database'    => 'heroku_2dfb529ea745083',
+        'prefix'      => ''
+    );
 }
