8fa37fdb68a2ad241f055c4ea4bf9f39ad2654e2
Tyson Ross <tross_github@tysonross.com>
2016-09-13 21:49:32 +1000

    rearranged conditional statement

--- app/Controller/CoursesController.php
+++ app/Controller/CoursesController.php
@@ -24,28 +24,42 @@ class CoursesController extends AppController {
 	public function index() {
 		$this->Course->recursive = 0;
 
-		// find only 10 day courses unless old student
-		$current_date = date('Y-m-d');
-		$is_old = $this->Course->Enrolment->find('count', array(
-				'conditions' => array(
-					'DATE(enrolment_date) < ' => $current_date,
-					'user_id' => AuthComponent::user('id'),
-					'Course.days' => 'ten'
-				))
-				) > 0;
 
-		// if not old, filter to only ten day courses
-		if (!$is_old) {
+		if (AuthComponent::user('id')) {
+			// find only 10 day courses unless old student
+			$current_date = date('Y-m-d');
+			$is_old = $this->Course->Enrolment->find('count', array(
+					'conditions' => array(
+						'DATE(enrolment_date) < ' => $current_date,
+						'user_id' => AuthComponent::user('id'),
+						'Course.days' => 'ten'
+					))
+					) > 0;
+
+			// if not old, filter to only ten day courses
+			if (!$is_old) {
+				$options = array(
+					'conditions' => array(
+						'Course.days = ' => 'ten'
+					)
+				);
+				$this->Paginator->settings = $options;
+			}
+
+			$this->set('courses', $this->Paginator->paginate('Courses'));
+
+		} else {
 			$options = array(
 				'conditions' => array(
 					'Course.days = ' => 'ten'
 				)
 			);
 			$this->Paginator->settings = $options;
+			$this->set('courses', $this->Paginator->paginate('Courses'));
 		}
 
 		
-		$this->set('courses', $this->Paginator->paginate('Courses'));
+		
 		
 
 		//$enrolments = $this->Course->Enrolment->find('list');
