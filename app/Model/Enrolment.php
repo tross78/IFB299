63343f546f216f5db882fe8fbc83bbe1cae369a5
Tyson Ross <tross_github@tysonross.com>
2016-09-15 20:29:41 +1000

    Added extra valid checks into course_id

--- app/Model/Enrolment.php
+++ app/Model/Enrolment.php
@@ -12,7 +12,15 @@ class Enrolment extends AppModel {
 	// The Associations below have been created with all possible keys, those that are not needed can be removed
 
 public $validate = array(
-		'Course.course_id' => array(
+		'course_id' => array(
+			'notBlank' => array(
+				'rule' => array('notBlank'),
+				'message' => 'A course_id is required'
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
 			'unique' => array(
 				'rule' => array('isUnique'),
 				'required' => 'create',
