059010a839b226fb08fd8201855729417f0da239
Tyson Ross <tross_github@tysonross.com>
2016-10-01 21:41:48 +1000

    using react for carousel

--- webroot/js/entry.js
+++ webroot/js/entry.js
@@ -1,3 +1,53 @@
 require("react");
 require("react-dom");
 require("react-bootstrap");
+
+const carouselInstance = (
+  <Carousel>
+    <Carousel.Item>
+      <img width={900} height={500} alt=" " src="http://i.imgur.com/NQpW6hm.jpg"/>
+      <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={900} height={500} alt=" " src="http://i.imgur.com/aau5RP1.jpg"/>
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={900} height={500} alt=" " src="http://i.imgur.com/eV91kxn.jpg"/>
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={900} height={500} alt=" " src="http://i.imgur.com/biVYjls.jpg"/>
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={900} height={500} alt=" " src="http://i.imgur.com/sZRaO9v.jpg"/>
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={900} height={500} alt=" " src="http://i.imgur.com/zQr7xWv.jpg"/>
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
+  
+);
+
+ReactDOM.render(carouselInstance, document.getElementById('feature-carousel'));
