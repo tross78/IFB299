360a336940adbbe14a442fd7de99990a086bfc5c
Tyson Ross <tross_github@tysonross.com>
2016-10-01 22:37:42 +1000

    forgot to import carouselitem

--- webroot/js/carousel.jsx
+++ webroot/js/carousel.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Carousel from 'react-bootstrap';
+import CarouselItem from 'react-bootstrap';
 const carouselInstance = (
   <Carousel>
     <Carousel.Item>
