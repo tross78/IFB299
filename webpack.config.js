0b403eb6ad8ddf33a48cac7d2572ce012ae5f22e
Tyson Ross <tross_github@tysonross.com>
2016-10-01 21:22:23 +1000

    jquery reference fix, temporary

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: ["./webroot/js/jquery.min.js", "./webroot/js/bootstrap.min.js"],
   output: {
     filename: "./webroot/js/bundle.js"
-  }
+  },
+  plugins: [
+        new webpack.ProvidePlugin({
+           $: "./jquery.min.js",
+           jQuery: "./jquery.min.js"
+       })
+    ]
 }
