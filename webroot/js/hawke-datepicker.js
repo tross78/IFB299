9f36e4e053d3767ec6d355f161155081b4782f1a
Tyson Ross <tross_github@tysonross.com>
2016-10-03 19:04:47 +1000

    rewrote sub-component

--- webroot/js/hawke-datepicker.js
+++ webroot/js/hawke-datepicker.js
@@ -1,9 +1,25 @@
-import React from 'react'
-import DatePicker from 'react-bootstrap-date-picker'
+require('react');
+require('react-bootstrap-date-picker');
 
-var HawkeDatePicker = function() {
-  return <div class="form-group">
+
+var HawkeDatePicker = React.createClass({
+  getInitialState: function(){
+    var value = new Date().toISOString();
+    return {
+      value: value
+    }
+  },
+  handleChange: function(value) {
+    // value is an ISO String.
+    this.setState({
+      value: value
+    });
+  },
+  render: function(){
+    return <div class="form-group">
       <DatePicker value={this.state.value} onChange={this.handleChange} inputProps={{ id:"UserDateOfBirth", name: "data[User][date_of_birth]", required:"required", placeholder:"Date of Birth"}} />
-    </div>
-}
+    </div>;
+  }
+});
+
 export default HawkeDatePicker;
