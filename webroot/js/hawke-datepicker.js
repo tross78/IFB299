b622f1dad0d0d8a4df2e5e735af889aa38626aa5
Tyson Ross <tross_github@tysonross.com>
2016-10-03 19:16:04 +1000

    ###

--- webroot/js/hawke-datepicker.js
+++ webroot/js/hawke-datepicker.js
@@ -17,7 +17,7 @@ var HawkeDatePicker = React.createClass({
   },
   render: function(){
     return <div class="form-group">
-      <DatePicker value={this.state.value} onChange={this.handleChange} inputProps={{ id:"UserDateOfBirth", name: "data[User][date_of_birth]", required:"required", placeholder:"Date of Birth"}} />
+      <DatePicker value={this.state.value} onChange={this.handleChange} inputProps={{ id:"UserDateOfBirth", name: "data[User][date_of_birth]", required:"required", placeholder:"Date of Birth", class:'form-group'}} />
     </div>;
   }
 });
--- webroot/js/hawke-password-strength.js
+++ webroot/js/hawke-password-strength.js
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactPasswordStrength from 'react-password-strength'
 
 var HawkePasswordStrength = function() {
-  return <div class="form-group"><ReactPasswordStrength
+  return <ReactPasswordStrength
   minLength={5}
   minScore={2}
   scoreWords={['weak', 'okay', 'good', 'strong', 'stronger']}
-  inputProps={{ id:"UserPassword", name: "data[User][password]", placeholder:"Password", autocomplete: "off", required:"required"}}
-/></div>
+  inputProps={{ id:"UserPassword", name: "data[User][password]", placeholder:"Password", autocomplete: "off", required:"required", class:'form-group'}}
+/>
 }
 export default HawkePasswordStrength;
