facbd9d253e64f43bc6cf06d36191faf7dab9069
Tyson Ross <tross_github@tysonross.com>
2016-10-03 19:00:32 +1000

    syntax was wrong

--- webroot/js/hawke-datepicker.js
+++ webroot/js/hawke-datepicker.js
@@ -2,10 +2,8 @@ import React from 'react'
 import DatePicker from 'react-bootstrap-date-picker'
 
 var HawkeDatePicker = function() {
-  return <FormGroup>
-      <ControlLabel>Label</ControlLabel>
+  return <div class="form-group">
       <DatePicker value={this.state.value} onChange={this.handleChange} inputProps={{ id:"UserDateOfBirth", name: "data[User][date_of_birth]", required:"required", placeholder:"Date of Birth"}} />
-      <HelpBlock>Help</HelpBlock>
-    </FormGroup>
+    </div>
 }
 export default HawkeDatePicker;
