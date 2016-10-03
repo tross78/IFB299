a188363b86557ec687ab1f6e14e8e88734faf8b5
Tyson Ross <tross_github@tysonross.com>
2016-10-03 20:08:59 +1000

    ###

--- webroot/js/hawke-datepicker.js
+++ webroot/js/hawke-datepicker.js
@@ -4,7 +4,7 @@ import DatePicker from 'react-bootstrap-date-picker';
 
 var HawkeDatePicker = React.createClass({
   getInitialState: function(){
-    var value = new Date().toISOString().slice(0, 11);
+    var value = new Date().toISOString().slice(0, 10);
     return {
       value: value
     }
