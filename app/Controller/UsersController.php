0dd0e1e8a94020dcd25a1cb842eca691d6c7a2b4
Tyson Ross <tross_github@tysonross.com>
2016-10-03 21:37:29 +1000

    ###

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -99,7 +99,6 @@ class UsersController extends AppController {
 	public function add() {
 		
 		if ($this->request->is('post')) {
-			$this->request->data['User']['date_of_birth'] = $this->request->data['User']['date_of_birth'].substr(0, 10);
 			$this->User->create();
 			if ($this->User->save($this->request->data)) {
 				$this->Flash->success(__('The user has been saved.'));
