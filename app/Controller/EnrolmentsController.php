bb897a87516d940cf0b5bbac1fd8f3febf4df4a3
Tyson Ross <tross_github@tysonross.com>
2016-10-05 11:35:30 +1000

    ###

--- app/Controller/EnrolmentsController.php
+++ app/Controller/EnrolmentsController.php
@@ -201,6 +201,8 @@ class EnrolmentsController extends AppController {
 		// if course_id set in params show just that course
 		if (isset($this->params['named']['course_id'])) {
 			$courses = $this->Enrolment->Course->find('list', array(
+				'fields' => array('Course.id', 'Course.start_date'
+				),
 				'conditions' => array(
 					"Course.id" => $this->params['named']['course_id'],
 				)
