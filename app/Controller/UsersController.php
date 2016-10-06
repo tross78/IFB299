ea79e633adbaa0b676ba946930566b64c26d8787
Jacob Martin <jama392@gmail.com>
2016-10-06 21:34:30 +1000

    termination check changed

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -54,12 +54,12 @@ class UsersController extends AppController {
 	}
 
 	public function login() {
-		if ($user['permission'] == 'terminated'){
+		if ($this->request->is('post')) {
+			if ($user['permission'] == 'terminated'){
 			$this->Flash->error(__('This account has been terminated by a manager. You will not be able to login.'));
-		}
+			}
 
-		elseif ($this->request->is('post')) {
-			if ($this->Auth->login()) {
+			elseif ($this->Auth->login()) {
 				return $this->redirect($this->Auth->redirectUrl());
 			}
 			$this->Flash->error(__('Invalid username or password, try again'));
