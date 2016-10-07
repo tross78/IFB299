6fabb7265e2d961bdd92d046136f268b947976e4
Jacob Martin <jama392@gmail.com>
2016-10-07 15:16:47 +1000

    Editing terminated users

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -53,6 +53,10 @@ class UsersController extends AppController {
 		return parent::isAuthorized($user);
 	}
 
+	public function logout() {
+		return $this->redirect($this->Auth->logout());
+	}
+
 	public function login() {
 		if ($this->request->is('post')) {
 			if ($this->Auth->login()) {
@@ -68,10 +72,6 @@ class UsersController extends AppController {
 			}
 			$this->Flash->error(__('Invalid username or password, try again'));
 		}
-	}
-
-	public function logout() {
-		return $this->redirect($this->Auth->logout());
 	}
 
 /**
