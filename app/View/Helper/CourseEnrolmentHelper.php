284ead215496446daa14e218ddd72eba88385677
Tyson Ross <tross_github@tysonross.com>
2016-10-13 10:10:24 +1000

    ####

--- app/View/Helper/CourseEnrolmentHelper.php
+++ app/View/Helper/CourseEnrolmentHelper.php
@@ -8,7 +8,7 @@ class CourseEnrolmentHelper extends AppHelper {
         $this->Enrolment = new Enrolment; 
         $this->data = $this->Enrolment->find('all', array(
 				'conditions' => array(
-					"Course.id" => $course_id,
+					"course_id" => $course_id,
 				)
 			));
         // create output.... 
