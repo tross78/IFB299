068276ade876aacb879019d1a3ee28cb5494255a
HGee <harrison.gee1@live.co.uk>
2016-10-14 18:52:07 +1000

    ugg

--- app/Controller/EnrolmentsController.php
+++ app/Controller/EnrolmentsController.php
@@ -214,11 +214,11 @@ class EnrolmentsController extends AppController {
 				if ($this->Enrolment->save($this->request->data)) {
 					$this->Flash->success(__('The enrolment has been saved.'));
 					if ($user_gender == 'male') {
-						$this->Enrolment->Course->enrolments_male + 1;
-						$this->Enrolment->Course->enrolments + 1;
+						$this->Enrolment->Course->enrolments_male =+ 1;
+						$this->Enrolment->Course->enrolments =+ 1;
 					} else {
-						$this->Enrolment->Course->enrolments_female + 1;
-						$this->Enrolment->Course->enrolments + 1;
+						$this->Enrolment->Course->enrolments_female =+ 1;
+						$this->Enrolment->Course->enrolments =+ 1;
 					}
 					return $this->redirect(array('action' => 'index'));
 				} else {
