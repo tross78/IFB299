9d54fd31e1e99d88b140e5500ded0e585574dec5
Andrew Grant <andr3wjgrant@gmail.com>
2016-10-19 04:27:56 +1000

    fix

--- app/Controller/CoursesController.php
+++ app/Controller/CoursesController.php
@@ -137,7 +137,7 @@ class CoursesController extends AppController {
 		$options = array('conditions' => array('Course.' . $this->Course->primaryKey => $id));
 		$this->set('course', $this->Course->find('first', $options));
 		
-		$this->set('enrolments', $this->Course->Enrolment->find('all', array(
+		$this->set('enrolments', $this->Course->User->find('all', array(
 			'fields' => array('Enrolment.id', 'Course.id', 'User.first_name','User.last_name', 'Enrolment.user_id', 'Enrolment.course_id', 'User.id'),
 			'contain' => array('Enrolment', 'User'),
 			'conditions' => array(
