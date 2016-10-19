81b43dba2e0dda5072a4e9d57b3c4cab1222d5ed
Jacob Martin <jama392@gmail.com>
2016-10-19 11:45:49 +1000

    fixing email content

--- app/Controller/CoursesController.php
+++ app/Controller/CoursesController.php
@@ -320,7 +320,7 @@ class CoursesController extends AppController {
 	  		$Email->from(array('teamhawkemeditation@gmail.com' => 'Hawke Meditation Centre'));
 	  		$Email->to($enrolledID['User']['email_address']);
 	  		$Email->subject('Changes to your Meditation Course');
-	  		$Email->send('Hi ' + $enrolledID['User']['first_name'] +'./n/nThe course you have enrolled in beggining on the' + $enrolledID['Course']['start_date'] + ' is no longer being continued. We are sorry for the inconvenience. /n/nTeam Hawke');
+	  		$Email->send('Hi ' . $enrolledID['User']['first_name'] .'./n/nThe course you have enrolled in beggining on the' . $enrolledID['Course']['start_date'] . ' is no longer being continued. We are sorry for the inconvenience. /n/nTeam Hawke');
 
 	  }
 
