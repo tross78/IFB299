3b43703dfdbf91ada558802e477a633ae83ff559
Jacob Martin <jama392@gmail.com>
2016-10-24 23:37:24 +1000

    small changes for dietary view - edited the courseenrolementhelper file to do so

--- app/View/Helper/CourseEnrolmentHelper.php
+++ app/View/Helper/CourseEnrolmentHelper.php
@@ -7,7 +7,7 @@ class CourseEnrolmentHelper extends AppHelper {
         App::import('Model', 'Enrolment'); 
         $this->Enrolment = new Enrolment; 
         $output = $this->Enrolment->find('all', array(
-                'fields' => array('Enrolment.id', 'Enrolment.user_id', 'Enrolment.course_id', 'User.id', 'User.first_name', 'User.last_name', 'User.dietary_requirements', 'User.medical_requirements'),
+                'fields' => array('Enrolment.id', 'Enrolment.user_id', 'Enrolment.course_id', 'User.id', 'User.first_name', 'User.last_name', 'User.dietary_requirements', 'User.medical_requirements', 'Enrolment.role'),
 				'contain' => array('User'),
 				'conditions' => array(
 					"course_id" => $course_id,
