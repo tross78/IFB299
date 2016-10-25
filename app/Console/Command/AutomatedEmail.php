7df2567309a7afc8822fd1676692b2e82880eaa2
Zane Tow <zanetow@gmail.com>
2016-10-25 22:37:11 +1000

    email update

--- app/Console/Command/AutomatedEmail.php
+++ app/Console/Command/AutomatedEmail.php
@@ -19,6 +19,5 @@ foreach ($userIDS as $userID) {
   $Email->to($userID['User']['email_address']);
   $Email->subject('Your course begins soon!');
   $Email->send('Hello ' . $userID['User']['first_name'] . ',' . "\n\n" . 'Sending a friendly reminder that your Meditation course begins in 10 days time on the ' . $userID['Course']['start_date'] . '.' . ' Due to a tendency in courses reaching full capacity, we require a response within 2 days time from having sent this email. Unfortunately if we don\'t receive a confirmation email you\'re spot in this course will be cancelled.' . "\n\n" . 'Thank you and we hope to see you soon!' . "\n\n" . '- The Hawke Centre Team');
-
-
+}
 ?>
--- sendemailjob.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-php -f app/Console/Command/AutomatedEmail.php
