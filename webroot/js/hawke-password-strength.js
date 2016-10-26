dc18e1138264abc53bb37b3e6a0cb552ec874d40
Tyson Ross <tross_github@tysonross.com>
2016-10-26 10:07:35 +1000

    reimplemented pw strength

--- webroot/js/hawke-password-strength.js
+++ webroot/js/hawke-password-strength.js
@@ -1,12 +1,10 @@
 import React from 'react'
+import ReactPasswordStrength from 'react-password-strength'
 
 var HawkePasswordStrength = function() {
-  return (
-    <div class="ReactPasswordStrength">
-      <input type="password" id="UserPassword" name="data[User][password]" placeholder="Password" required="" value="password"/>
-      <div class="ReactPasswordStrength-strength-bar"></div>
-      <span class="ReactPasswordStrength-strength-desc"></span>
-    </div>
-  )
+  return <div class="form-group">
+        <DatePicker value={this.state.value} onChange={this.handleChange} inputProps={{ id:"UserDateOfBirth", name: "data[User][date_of_birth]", required:"required", placeholder:"Date of Birth"}} />
+        <DatePicker value={this.state.value} onChange={this.handleChange} inputProps={{ id:"UserDateOfBirth", name: "data[User][date_of_birth]", required:"required", placeholder:"Date of Birth", class:'form-group'}} />
+        </div>;
 }
 export default HawkePasswordStrength;
